0012261a65ca24d90a37c07c397579dbd5620aa7
tsweets <tony.sweets@gmail.com>
2023-05-01 08:55:26 -0600

    1st checkin

---
+++ src/main/java/org/beer30/realworld/SpingRealWorldApplication.java
@@ -0,0 +1,13 @@
+package org.beer30.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingRealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingRealWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/beer30/realworld/SpingRealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.beer30.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingRealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
